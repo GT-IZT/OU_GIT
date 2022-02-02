21ba9e0aeba51be099c6f2251f64894dca45732c
abc <240827@edu.p.lodz.pl>
2022-02-02 18:35:32 +0100

    (zmiana na mnożenie w metodzie policz).

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
+int Policz (int a, int b) {
+    Return a*b;
+}
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    std:: cout <<”W pierwszym branchu; 2+3=” << (Policz2,3) <<std::endl;
     return 0;
 }
